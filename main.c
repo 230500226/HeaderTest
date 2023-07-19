aea7ec7dbfe74770e68b00627df529e9344cb542
Shahied Rustin <97807830+snowyru@users.noreply.github.com>
2023-07-19 20:04:15 +0200

    added multiple file support

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 //local header file 
     //remember to use make or have command add the extra c file manually or automatically with *.c
+
+    //code runner execution map :
+    //"code-runner.executorMap": {
+    //  "c": "cd $dir ; if ($?) { gcc $fileNameWithoutExt.c -o $fileNameWithoutExt -lpdcurses -DNCURSES_STATIC} ; if ($?) { .\\$fileNameWithoutExt }"
+    //}
+    // use this when compiling user headers
+    // "code-runner.executorMap": {
+    //      "c": "cd $dir ; if ($?) { gcc *.c -o $fileNameWithoutExt -lpdcurses -DNCURSES_STATIC} ; if ($?) { .\\$fileNameWithoutExt }"
+    //  }
+
 #include "library.h"
 
 int main(){
@@ -8,6 +18,7 @@ int main(){
     int a = 1, b = 2;
 
     printf("%d\n",add(a,b));
+    
     printf("%d\n",sub(a,b));
 
 return 0;}
